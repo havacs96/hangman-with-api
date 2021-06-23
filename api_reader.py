b2fa0135edfc3fd98084a0f6a0d091bbe9895ff8
havacs96 <havacs.peter@gmail.com>
2021-06-23 19:29:59 +0200

    feat: created is_unused validator

--- api_reader.py
+++ api_reader.py
@@ -4,6 +4,7 @@ def get_api_data():
     word_from_api = ""
     response = requests.get("http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
     for word_dict in response.json():
+        print(word_dict)
         word_from_api += word_dict.get("word")
     return word_from_api
     
