bc80ad04c2425f2ec549aced3ef63f667f117f25
havacs96 <havacs.peter@gmail.com>
2021-07-12 20:37:49 +0200

    feat: created sql query for listing all players

--- data_handler.sh
+++ data_handler.sh
@@ -6,3 +6,20 @@ export USERNAME=Teszta_123
 export HOsTNAME=localhost:5432
 export PGPASSWORD=Teszta_123
 
+list_players() {
+psql << EOF
+SELECT user_name AS "Player name", score AS "Score", difficulty AS Difficulty FROM score
+EOF
+}
+
+main() {
+    if [[ "$1" == "list-players" ]]
+    then
+        list_players
+    fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+    main "$@"
+fi
