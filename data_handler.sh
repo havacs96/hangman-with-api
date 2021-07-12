db3ecc9f4108edf65cef7284807fbbdedcec3345
havacs96 <havacs.peter@gmail.com>
2021-07-12 20:39:12 +0200

    feat: created sql query for adding player

--- data_handler.sh
+++ data_handler.sh
@@ -6,6 +6,13 @@ export USERNAME=Teszta_123
 export HOsTNAME=localhost:5432
 export PGPASSWORD=Teszta_123
 
+add_player() {
+psql <<EOF
+INSERT INTO score (user_name, score, difficulty)
+VALUES ('$1', '$2', '$3')
+EOF
+}
+
 list_players() {
 psql << EOF
 SELECT user_name AS "Player name", score AS "Score", difficulty AS Difficulty FROM score
@@ -13,7 +20,10 @@ EOF
 }
 
 main() {
-    if [[ "$1" == "list-players" ]]
+    if [[ "$1" == "add-player" ]]
+    then
+        add_player "$2" "$3" "$4"
+    elif [[ "$1" == "list-players" ]]
     then
         list_players
     fi
